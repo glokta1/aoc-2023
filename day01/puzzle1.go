dde0a5fdc2723c96ce24d6fece9e14268554d1cc
Rafey Ahmad <rafeyahmad@protonmail.com>
2023-12-05 01:02:44 +0530

    almost at the finish line for day01part2

--- day01/puzzle1.go
+++ day01/puzzle1.go
@@ -3,37 +3,76 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
 	"strconv"
 
 	"github.com/glokta1/aoc-2023/cio"
 )
 
+var m = map[string]string{
+	"one":   "1",
+	"two":   "2",
+	"three": "3",
+	"four":  "4",
+	"five":  "5",
+	"six":   "6",
+	"seven": "7",
+	"eight": "8",
+	"nine":  "9",
+	"zero":  "0",
+}
+
+func getMapKeys(m map[string]string) []string {
+	keys := make([]string, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	return keys
+}
+
+func generateRegexPattern() string {
+	pattern := "?=\\d"
+	nums := getMapKeys(m)
+	for _, numString := range nums {
+		pattern += fmt.Sprintf("|%s", numString)
+	}
+
+	return pattern
+}
+
 func main() {
+	patternString := generateRegexPattern()
 	lines, err := cio.ReadLines("input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	sum := 0
-	for i, line := range lines {
-		cnt, first, last := 0, string(line[0]), string(line[0])
-		for _, char := range line {
-			if char >= '0' && char <= '9' {
-				cnt++
-				if cnt == 1 {
-					first = string(char)
-				}
-
-				last = string(char)
-			}
+	for idx, line := range lines {
+		fmt.Println(idx, line)
+		pattern, err := regexp.Compile(patternString)
+		if err != nil {
+			log.Fatal(err)
 		}
 
-		res, _ := strconv.Atoi(first + last)
-		sum += res
+		matches := pattern.FindAllString(line, -1)
+		first, last := matches[0], matches[len(matches)-1]
+		if val, ok := m[first]; ok {
+			first = val
+		}
+		if val, ok := m[last]; ok {
+			last = val
+		}
 
-		fmt.Println(i, first, last, sum)
+		res, err := strconv.Atoi(first + last)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		sum += res
+		fmt.Println(first, last, res, sum)
 	}
 
-	fmt.Println(sum)
-
+	// fmt.Println(sum)
 }
